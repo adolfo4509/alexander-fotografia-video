de9008b721a90294db70041ab598d15662fa60ba
adolfo <calvo330906@gmail.com>
2026-08-29 18:27:03 -0500

    Se realizan avances de enrutado

--- alexander-fotografia-video/src/app/page.tsx
+++ alexander-fotografia-video/src/app/page.tsx
@@ -1,25 +1,41 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 export default function HomePage() {
+  const router = useRouter();
+
+  const navigateToSessions = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    
+    router.push( e.currentTarget.innerText === "Ver sesiones" ?  "/sessions" : "/gallery");
+  };
+
   return (
     <section className="space-y-4">
-      <h1 className="text-3xl font-bold">PhotoCloud Studios</h1>
+      <h1 className="text-3xl font-bold">Alexander Fotografía y Video</h1>
       <p className="text-lg text-slate-300">
         Plataforma de gestión fotográfica en la nube.
       </p>
 
       <div className="space-x-4">
-        <a
+        <Link 
           href="/sessions"
+          onClick={(e) => navigateToSessions(e)}
           className="bg-indigo-600 px-4 py-2 rounded text-white"
+          
         >
           Ver sesiones
-        </a>
+        </Link>
 
-        <a
+        <Link
           href="/gallery"
+          onClick={(e) => navigateToSessions(e)}
           className="bg-slate-700 px-4 py-2 rounded text-white"
         >
           Galerías
-        </a>
+        </Link>
       </div>
     </section>
   );
